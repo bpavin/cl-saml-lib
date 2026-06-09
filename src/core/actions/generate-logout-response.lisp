e348e93339d7fcd3ad4931e969f5b598f153da7f
Borna P <bpavin1@gmail.com>
2026-06-09 14:04:57 +0200

    generate-logout-response: added generating signature

--- src/core/actions/generate-logout-response.lisp
+++ src/core/actions/generate-logout-response.lisp
@@ -16,15 +16,24 @@
   ((idp-config :type idp-config:idp-config)
    (crypto-provider :type crypto-provider:crypto-provider)))
 
-(defmethod run ((this generate-logout-response) &key idp-config in-response-to status)
+(defmethod run ((this generate-logout-response) &key idp-config sign-response in-response-to status)
   "Generate a SAML LogoutResponse XML string.
 THIS: generate-logout-response instance
 IN-RESPONSE-TO: string - the ID of the LogoutRequest being responded to (required)
 STATUS: optional saml-status instance (default: success status)
 Returns: LogoutResponse XML string"
-  (logout-response:generate-logout-response-xml
-   idp-config
-   :in-response-to in-response-to
-   :status (when status
-             (make-instance 'saml-status:saml-status
-                            :status-code status))))
+  (let* ((response (logout-response:generate-logout-response-from-config
+                    idp-config
+                    :in-response-to in-response-to
+                    :status (when status
+                              (make-instance 'saml-status:saml-status
+                                             :status-code status))))
+         (xml (logout-response:build-logout-response-xml response)))
+    (if sign-response
+        (setf xml
+              (crypto-provider:sign-xml (crypto-provider this)
+                                        xml (format nil "//*[@ID='~A']" (logout-response:id response))
+                                        (idp-config:idp-private-key (idp-config this))
+                                        (idp-config:idp-certificate (idp-config this)))))
+
+    xml))
